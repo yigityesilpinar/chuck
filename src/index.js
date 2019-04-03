3f9cd1c2ca3f3bc258817045fab0102cf7cc6a9c
sidletsky <oleksandr.sidletskyi@gmail.com>
2019-04-03 14:09:21 +0200

    github pages deployment

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router } from 'react-router';
+import { Route } from 'react-router-dom';
+import { createHashHistory } from 'history';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const hashHistory = createHashHistory({ basename: process.env.PUBLIC_URL });
+
+ReactDOM.render(
+  <Router history={hashHistory}>
+    <Route path="/" component={App} />
+  </Router>,
+  document.getElementById('root')
+);
