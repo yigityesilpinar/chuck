b3b989a4ea218d2d60222e01526f6de0ababc4b3
sidletsky <oleksandr.sidletskyi@gmail.com>
2019-04-04 20:39:46 +0200

    key codes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, { Component, Fragment } from 'react';
 import styled from 'styled-components';
+import RCSelect from 'react-select';
+
 import { getRandomJoke } from 'api/jokes';
 import { getCategories } from 'api/categories';
-import RCSelect from 'react-select';
+
+import { keyCodes } from 'src/dict';
 
 const FullScreen = styled('div')`
   align-items: center;
@@ -65,11 +68,12 @@ class App extends Component {
   };
 
   handleKeyboardDown = ({ keyCode }) => {
-    if ((keyCode >= 65 && keyCode <= 90) || keyCode === 32) {
-      // letter or space bar
+    if (
+      (keyCode >= keyCodes.KeyA && keyCode <= keyCodes.KeyZ) ||
+      keyCode === keyCodes.Space
+    ) {
       this.getJoke(this.state.selectedCategory.value);
-    } else if (keyCode >= 37 && keyCode <= 40) {
-      // arrows
+    } else if (keyCode >= keyCodes.ArrowLeft && keyCode <= keyCodes.ArrowDown) {
       this.onArrowChangeCategory(keyCode);
     }
   };
@@ -79,13 +83,16 @@ class App extends Component {
     const currentCategoryIndex = categories
       .map(cat => cat.value)
       .indexOf(selectedCategory.value);
-    if (keyCode === 38 || keyCode === 37) {
+    if (keyCode === keyCodes.ArrowUp || keyCode === keyCodes.ArrowLeft) {
       if (currentCategoryIndex > 0) {
         this.changeOptionCategory(categories[currentCategoryIndex - 1]);
       } else {
         this.changeOptionCategory(categories[categories.length - 1]);
       }
-    } else if (keyCode === 39 || keyCode === 40) {
+    } else if (
+      keyCode === keyCodes.ArrowRight ||
+      keyCode === keyCodes.ArrowDown
+    ) {
       if (currentCategoryIndex === categories.length - 1) {
         this.changeOptionCategory(categories[0]);
       } else {
---
+++ src/dict.js
@@ -0,0 +1,9 @@
+export const keyCodes = {
+  ArrowLeft: 37,
+  ArrowUp: 38,
+  ArrowRight: 39,
+  ArrowDown: 40,
+  KeyA: 65,
+  KeyZ: 90,
+  Space: 32
+};
