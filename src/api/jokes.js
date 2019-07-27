423a567c36fbe18668b4680b43c9f0dd6caa9c56
sidletsky <9529998+sidletsky@users.noreply.github.com>
2019-07-27 16:17:28 +0200

    Update jokes.js

--- src/api/jokes.js
+++ src/api/jokes.js
@@ -1,7 +1,6 @@
 import axios from 'api';
 
-// async needs await, this guy already returning a promise
-export const getRandomJoke = async category =>
+export const getRandomJoke = category =>
   axios.get(`jokes/random?category=${category}`);
 
 export const getRandomJokes = async category =>
